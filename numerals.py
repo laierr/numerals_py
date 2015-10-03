bd438da9a6ce4adecfddbc50090a5c9182ef5121
laierr <kenderberator@gmail.com>
2015-10-04 01:27:01 +0300

    double digits

--- numerals.py
+++ numerals.py
@@ -1,4 +1,53 @@
-def selftest():
-	return True
+units = {
+	0: "efes",
+	1: "ehad",
+	2: "shtaim",
+	3: "shelosh",
+	4: "arba",
+	5: "hamesh",
+	6: "shesh",
+	7: "sheva",
+	8: "shmone",
+	9: "teisha"
+}
 
+tens = {
+	1: "eser",
+	2: "esrim",
+	3: "shloshim",
+	4: "arbaim",
+	5: "hameshim",
+	6: "shishim",
+	7: "shevim",
+	8: "shmonim",
+	9: "teshim"
+}
 
+hundreds = {
+	1: "mea",
+	2: "mataim",
+	3: "shlosh meot",
+	4: "arba meot",
+	5: "hamesh meot",
+	6: "shesh meot",
+	7: "shva meot",
+	8: "shmona meot",
+	9: "tsha meot"
+}
+
+def number_to_txt (numeral):
+	array = list(str(numeral))
+	array = map(lambda x: int(x), array)
+
+	if len(array) == 1:
+		return units[numeral]
+	elif len(array) == 2:
+		return tens_to_txt(array[0], array[1])
+
+def tens_to_txt(ten, unit):
+	if unit == 0:
+		return tens[ten]
+	elif ten == 1:
+		return units[unit] + "esre"
+
+	return tens[ten] + " ve"+ units[unit]
--- test/test.py
+++ test/test.py
@@ -4,17 +4,31 @@ import sys
 sys.path.insert(0, '../')
 import numerals
 
-class Selftest (unittest.TestCase):
-	def test(self):
-		assert True
-	
-	def test_import(self):
-		assert numerals.selftest()
+class NumeralsTest (unittest.TestCase):
+	def test_we_have_dict(self):
+		self.assertEqual(numerals.units[0], "efes")
+
+	def test_we_convert_units_to_text(self):
+		self.assertEqual(numerals.number_to_txt(1), "ehad")
+
+	def test_we_convert_ten_to_text(self):
+		self.assertEqual(numerals.number_to_txt(10), "eser")
+
+	def test_we_convert_tens_to_text(self):
+		self.assertEqual(numerals.number_to_txt(11), "ehadesre")
+
+	def test_we_connvert_twenties_to_text(self):
+		self.assertEqual(numerals.number_to_txt(22), "esrim veshtaim")
+
+
+
+
+
+
 
 
 def main():
     unittest.main()
-
 
 if __name__ == '__main__':
 	main()

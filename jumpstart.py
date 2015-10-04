d8196638d2a9ad4be87d3a0fa7eae121a2616e22
laierr <kenderberator@gmail.com>
2015-10-04 16:57:10 +0300

    refactoring

--- jumpstart.py
+++ jumpstart.py
@@ -4,16 +4,18 @@ import numerals
 rnd = randint(0,999)
 print numerals.number_to_txt(rnd)
 
-done = False
-
-while not done:
+def jumpstart():
 	raw_answer = raw_input('Your answer: ')
 	if raw_answer.isdigit():
 		answer = int(raw_answer)
 		if answer == rnd:
 			print 'CORRECT!'
-			done = True
 		else:
 			print 'No luck, try again!'
+			jumpstart()
 	else:
-		print 'INTEGER, pleese!'
+		print 'INTEGER, please!'
+		jumpstart()
+
+if __name__ == '__main__':
+	jumpstart()
